c500b806bbe27a7039a8edc73a59f0c46149e31b
Pierre Ngo Thibodeaux <pthibodeaux@midway2-login2.rcc.local>
2024-02-25 12:29:20 -0600

    Changes

--- fit_vdisp.py
+++ fit_vdisp.py
@@ -20,6 +20,7 @@ from vdisp import medcall
 dic,n= medcall(data_path,ncall=nnn,sort=False)
 sigma=dic['sigma']
 
-with open(outfile,'a') as of:
-    of.write(f'{sigma[0]:>6.3f} {sigma[1]:>6.3f} {sigma[2]:>6.3f} {n:>4} {obstime:>6}\n')
+if n==nnn:
+    with open(outfile,'a') as of:
+        of.write(f'{sigma[0]:>6.3f} {sigma[1]:>6.3f} {sigma[2]:>6.3f} {n:>4} {obstime:>6}\n')
 
